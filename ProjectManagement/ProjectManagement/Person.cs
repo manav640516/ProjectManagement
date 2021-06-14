254f6f71075e188082ba647693dabf8f8980cb6d
manavpadda11@gmail.com <manavpadda11@gmail.com>
2021-06-14 21:40:46 +0300

    Add project files.

---
+++ ProjectManagement/ProjectManagement/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Passwrd { get; set; }
+    }
+}
